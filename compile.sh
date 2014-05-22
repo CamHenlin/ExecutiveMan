e8bcc6b5c7bb3532294096238d292713e50c5263
Cameron Henlin <cameron@ctl-mac-074.local>
2014-05-22 11:22:13 -0700

    fixed compile script

--- compile.sh
+++ compile.sh
@@ -1,1 +1,20 @@
-compile script broken java -jar compiler.jar --js=easeljs-NEXT.min.js --js=soundjs-0.5.2.min.js --js=preload-NEXT.min.js --js=basiccollision.js --js=collision.js --js=fastCollision.js --js=renderer.js --js=healthbar.js --js=accountingmanlevel.js --js=wastemanlevel.js --js=door.js --js=platform.js --js=droppingplatform.js --js=disappearingplatform.js --js=bosshealthbar.js --js=killcopy.js  --js=wasteman.js --js=printerguy.js --js=flood.js --js=bighealth.js --js=littlehealth.js --js=shieldguy.js --js=copter.js --js=filingcabinet.js --js=player.js --js=titlescreen.js --js=bossscreen.js --js=showoffboss.js --js=pausemenu.js --js=shopmenu.js  --js=bosspoint.js  --js=halfwaypoint.js  --js=main.js --js=soundwrapper.js --js_output_file=game.min.js
+java -jar compiler.jar \
+	--js=libs/easeljs-NEXT.min.js --js=libs/soundjs-0.5.2.min.js --js=libs/preload-NEXT.min.js \
+	\
+	--js=levels/wastemanlevel.js --js=levels/accountingmanlevel.js \
+	\
+	--js=objects/healthbar.js --js=objects/bosspoint.js --js=objects/door.js \
+	--js=objects/platform.js --js=objects/droppingplatform.js --js=objects/disappearingplatform.js \
+	--js=objects/bosshealthbar.js --js=objects/bighealth.js --js=objects/littlehealth.js \
+	--js=objects/halfwaypoint.js \
+	\
+	--js=enemies/killcopy.js --js=enemies/wasteman.js --js=enemies/printerguy.js \
+	--js=enemies/flood.js --js=enemies/shieldguy.js --js=enemies/copter.js \
+	--js=enemies/filingcabinet.js \
+	\
+	 --js=mechanics/main.js --js=mechanics/collision.js --js=mechanics/fastCollision.js \
+	--js=mechanics/renderer.js --js=mechanics/player.js  --js=mechanics/titlescreen.js \
+	--js=mechanics/pausemenu.js --js=mechanics/shopmenu.js --js=mechanics/showoffboss.js \
+	--js=mechanics/bossscreen.js --js=mechanics/soundwrapper.js --js=mechanics/basiccollision.js \
+	\
+	--js_output_file=game.min.js
