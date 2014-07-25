46f26ec2a6d26be18b536bc4b8335c29ef499d3f
Cameron Henlin <cameron@d57-120.uoregon.edu>
2014-07-25 14:56:54 -0700

    fixed typo in makelevel.sh

--- leveljson/makelevel.sh
+++ leveljson/makelevel.sh
@@ -7,7 +7,7 @@ touch ../levels/$1level.js
 echo "building new level $1level.js with $((FILECOUNT)) screens."
 echo "var $1HalfwayPoint = 0;" >> ../levels/$1level.js
 echo "var $1BossPoint = 0;" >> ../levels/$1level.js
-echo "var $1manmaps = [];" >> ../levels/$1level.js
+echo "var $1maps = [];" >> ../levels/$1level.js
 echo "" >> ../levels/$1level.js
 
 for i in `seq 1 $((FILECOUNT))`
