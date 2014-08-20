d945431c074f62b6ec6b2f6e89dbfb8893917872
Cameron Henlin <cameron@ctl-mac-074.local>
2014-08-19 23:34:28 -0700

    wondre

--- mechanics/optionsmenu.js
+++ mechanics/optionsmenu.js
@@ -1,4 +1,4 @@
-var pauseUp = false;
+var optionsUp = false;
 function OptionsMenu() {
 	pauseUp = false;
 	pausestage = new createjs.Container();
@@ -18,85 +18,36 @@ function OptionsMenu() {
 
 	var executivemanLabel = new createjs.Text("EXECUTIVE MAN", "bold 7px Arial", "#FFF");
 	var executivemanLabel2 = new createjs.Text("EXECUTIVE MAN", "bold 7px Arial", "#000");
-	var exitStageLabel = new createjs.Text("EXIT STAGE", "bold 7px Arial", "#FFF");
 	var livesLabel = new createjs.Text("", "bold 8px Arial", "#FFF");
 
 	var healthBriefCasesLabel = new createjs.Text("", "bold 8px Arial", "#FFF");
 
 
-	var extraLifeSpriteSheet = new createjs.SpriteSheet({
-		"images": [loader.getResult("extralife")],
-		"frames": {
-			"width": 12, "height": 13, "count": 1
-		},
-		"animations": {
-			"shot": {
-				"frames" : [0],
-				"next" : "still"
-			}
-		}
-	});
-
-	var extraLifeSprite = new createjs.Sprite(extraLifeSpriteSheet, "still");
-
-	var healthBriefCaseSpriteSheet = new createjs.SpriteSheet({
-		"images": [loader.getResult("healthbriefcase")],
-		"frames": {
-			"width": 12, "height": 13, "count": 2
-		},
-		"animations": {
-			"still": {
-				"frames" : [0, 1],
-				"next" : "still",
-				"speed" : 0.05
-			}
-		}
-	});
-	var healthBriefCasesSprite = new createjs.Sprite(healthBriefCaseSpriteSheet, "still");
-
 	executivemanLabel.x = gamestage.canvas.width / 2 - 30;
 	executivemanLabel.y = 38;
 	executivemanLabel2.x = gamestage.canvas.width / 2 - 29;
 	executivemanLabel2.y = 39;
-	exitStageLabel.x = gamestage.canvas.width / 2 + 8;
-	exitStageLabel.y = gamestage.canvas.height - 44;
-	livesLabel.x = 64;
-	livesLabel.y = gamestage.canvas.height - 46;
-	extraLifeSprite.x = 42;
-	extraLifeSprite.y = gamestage.canvas.height - 49;
-
-	healthBriefCasesLabel.x = 64;
-	healthBriefCasesLabel.y = gamestage.canvas.height - 60;
-	healthBriefCasesSprite.x = 42;
-	healthBriefCasesSprite.y = gamestage.canvas.height - 65;
-	this.healthBriefCasesTouchTarget = buildTouchTarget(healthBriefCasesLabel);
 
     pausestage.addChild(shape);
     pausestage.addChild(executivemanLabel2);
     pausestage.addChild(executivemanLabel);
-    pausestage.addChild(this.exitStageTouchTarget);
-    pausestage.addChild(exitStageLabel);
-    pausestage.addChild(livesLabel);
-    pausestage.addChild(extraLifeSprite);
-    pausestage.addChild(healthBriefCasesLabel);
-    pausestage.addChild(healthBriefCasesSprite);
     pausestage.addChild(divider);
 
     this.remove = function() {
-    	pauseUp = false;
+		optionsUp = false;
         playSound("pauseclose");
-    	gamestage.removeChild(pausestage);
+		gamestage.removeChild(pausestage);
     };
 
     this.show = function() {
-    	pauseUp = true;
-        playSound("pauseopen");
+		optionsUp = true;
+		playSound("pauseopen");
 
 		document.addEventListener('click', pauseClickHandler.bind(this), false);
 
-    	livesLabel.text = lives;
-    	healthBriefCasesLabel.text = healthBriefCases;
-    	gamestage.addChild(pausestage);
+		livesLabel.text = lives;
+		healthBriefCasesLabel.text = healthBriefCases;
+		gamestage.addChild(pausestage);
     };
 }
 
@@ -137,18 +88,17 @@ function pauseClickHandler(event) {
 
     touchSprite.x = event.clientX / gamezoom;
     touchSprite.y = event.clientY / gamezoom;
-    if (fastCollisionSprite(this.exitStageTouchTarget, touchSprite)) {
+/*    if (fastCollisionSprite(this.exitStageTouchTarget, touchSprite)) {
 		lives = -1;
 		player.health = 0;
 		player.paused = false;
 		player.pauseMenu.remove();
 		event.target.removeEventListener(event.type, arguments.callee, false);
     } if (fastCollisionSprite(this.healthBriefCasesTouchTarget, touchSprite)) {
-    	if (healthBriefCases > 0) {
-    		healthBriefCases--;
-    		player.health = 28;
-    		this.remove();
-    		this.show();
-    	}
-    }
+	if (healthBriefCases > 0) {
+		healthBriefCases--;
+		player.health = 28;
+		this.remove();
+		this.show();
+	}*/
 }
