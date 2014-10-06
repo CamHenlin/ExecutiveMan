f7f7d78cff0726d2503b46369222d1221588604d
Cameron Henlin <cameron@d56-193.uoregon.edu>
2014-10-06 15:33:12 -0700

    magical smallness addition

--- mechanics/soundwrapper.js
+++ mechanics/soundwrapper.js
@@ -16,33 +16,33 @@ createjs.Sound.registerSound("sounds/warehousemanshoot.wav", "warehousemanshoot"
 createjs.Sound.registerSound("sounds/warehousemanshothit.wav", "warehousemanshothit");
 createjs.Sound.registerSound("sounds/warehousemanshothitsmall.wav", "warehousemanshothitsmall");
 
-createjs.Sound.registerSound("songs/intro.mp3", "intro");
-createjs.Sound.registerSound("songs/title.mp3", "title");
-createjs.Sound.registerSound("songs/wasteman.mp3", "wastemansong");
-createjs.Sound.registerSound("songs/accountingman.mp3", "accountingmansong");
-createjs.Sound.registerSound("songs/warehouseman.mp3", "warehousemansong");
-createjs.Sound.registerSound("songs/materialman.mp3", "materialmansong");
-createjs.Sound.registerSound("songs/visionaryman.mp3", "visionarymansong");
+createjs.Sound.registerSound("songs/intro_small.mp3", "intro");
+createjs.Sound.registerSound("songs/title_small.mp3", "title");
+createjs.Sound.registerSound("songs/wasteman_small.mp3", "wastemansong");
+createjs.Sound.registerSound("songs/accountingman_small.mp3", "accountingmansong");
+createjs.Sound.registerSound("songs/warehouseman_small.mp3", "warehousemansong");
+createjs.Sound.registerSound("songs/materialman_small.mp3", "materialmansong");
+createjs.Sound.registerSound("songs/visionaryman_small.mp3", "visionarymansong");
 
 loader.installPlugin(createjs.SoundJS);
 
 function playSound(sound) {
 	if (soundOff) { return; }
     var fx = createjs.Sound.play(sound);
-    fx.volume = 0.25;
+    fx.volume = 0.4;
 }
 
 function playSoundTwice(sound) {
 	if (soundOff) { return; }
 	var loop = createjs.Sound.play(sound, {loop:1});
-	loop.volume = 0.25;
+	loop.volume = 0.4;
 }
 
 function playSoundLoop(sound) {
 	if (musicOff || soundOff) { return; }
 
 	var loop = createjs.Sound.play(sound, {loop:-1});
-	loop.volume = 0.15;
+	loop.volume = 0.25;
 }
 
 function stopMusic() {
