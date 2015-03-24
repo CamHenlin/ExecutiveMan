2185a8f5f7934d7990ee8f4556394be1e8d2ab11
Cameron Henlin <cam.henlin@gmail.com>
2015-03-24 10:55:17 -0700

    added missing files. oops.

---
+++ mechanics/globals.js
@@ -0,0 +1,170 @@
+//
+var altstage;
+
+//
+var bossframes = [];
+
+//
+var bossNumber = 0;
+
+//
+var bossnumber = 0;
+
+//
+var bossscreenCounter = 0;
+
+//
+var bossScreenUp = false;
+
+//
+var buttonSpriteSheet;
+
+//
+var clicked = false;
+
+//
+var damageModifier = 1;
+
+//
+var dead = true;
+
+//
+var demoEnded = false;
+
+//
+var dialogstage;
+
+//
+var doubleJump = false;
+
+//
+var executivemanLoadingText = new createjs.Text("Executive Man loading...", "14px '" + FONT + "'", "#FFF");
+
+//
+var explosionSprite;
+
+//
+var fpsLabel;
+
+//
+var gamestage;
+
+//
+var gamezoom = 2;
+
+//
+var healthBriefCases = 0;
+
+//
+var healthModifier = 1;
+
+//
+var isDemo = false;
+
+//
+var itemDropCount = 0;
+
+//
+var leftButtonSprite;
+
+//
+var lives = 2;
+
+//
+var loadProgress = 0;
+
+//
+var logFPS = false;
+
+//
+var mainMenuScreenUp = false;
+
+//
+var menuUp = false;
+
+//
+var mobile = false;
+
+//
+var musicOff = false;
+
+//
+var optionsstage;
+
+//
+var optionsUp = false;
+
+//
+var pauseButtonSprite;
+
+//
+var pausestage;
+
+//
+var pauseUp = false;
+
+//
+var player;
+
+//
+var progress;
+
+//
+var renderer;
+
+//
+var rightButtonSprite;
+
+//
+var score = 0;
+
+//
+var scoreLabel;
+
+//
+var scoreModifier = 1;
+
+//
+var selectionNumber = 0;
+
+//
+var shootButtonSprite;
+
+//
+var shopUp = false;
+
+//
+var shotExplosionSprite;
+
+//
+var showOffBossScreenCounter = 0;
+
+//
+var skipCounter = 0;
+
+//
+var skipFrames = 1;
+
+//
+var soundOff = false;
+
+//
+var stage;
+
+//
+var startbossscreen = false;
+
+//
+var startgame;
+
+//
+var startlevel = false;
+
+//
+var tileCollisionDetector;
+
+//
+var titleScreenSprite;
+
+//
+var watchedElements;
