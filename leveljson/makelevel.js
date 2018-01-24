583dc960705df2b7c1210d950723bed54e3917a5
charleskilbourne <charleskilbourne@yahoo.com>
2018-01-24 07:07:04 -0800

    basic build complete

--- leveljson/makelevel.js
+++ leveljson/makelevel.js
@@ -1,29 +1,23 @@
 const glob = require('glob');
 const fs = require('fs');
-const args = process.argv;
 
+module.exports.buildLevel = function(levelName, halfwayPoint) {
+    const levelJsonPath = `leveljson/${levelName}`;
+    const levelConfig = glob.sync(levelJsonPath + '*');
+    const levelpath = 'levels/' + levelName + 'level.js'
 
-const levelName = args[2];
-const halfwaypoint = args[3] || 0;
-const levelJsonPath = `leveljson/${levelName}`;
-const levelConfig = glob.sync(levelJsonPath + '*');
-const levelpath = 'levels/' + levelName + 'level.js'
+    let levelData = "";
 
-let levelData = "";
+    levelData += `var ${levelName}HalfwayPoint = ${halfwayPoint};\n`;
+    levelData += `var ${levelName}BossPoint = ${levelConfig.length - 2};\n`;
+    levelData += `var ${levelName}maps = [];\n\n`;
 
-levelData += `var ${levelName}HalfwayPoint = ${halfwaypoint};\n`;
-levelData += `var ${levelName}BossPoint = ${levelConfig.length - 2};\n`;
-levelData += `var ${levelName}maps = [];\n\n`;
+    let file;
+    for(let i = 0; i < levelConfig.length; i++){
+        file = fs.readFileSync(levelJsonPath + (i + 1) + '.json', 'utf-8');
+        levelData += `${levelName}maps[${i}] = ${file.replace(/\s/g,'')}\n`;
+    }
 
-
-let file;
-for(let i = 0; i < levelConfig.length; i++){
-    file = fs.readFileSync(levelJsonPath + (i + 1) + '.json', 'utf-8');
-    console.log(file);
-    levelData += `${levelName}maps[${i}] = ${file.replace(/\s/g,'')}\n`;
+    const levelFile = fs.writeFileSync(levelpath, levelData);
+    console.log(`Successfully built ${levelName}level.js with ${levelConfig.length + 1} screens.`)
 }
-
-const levelFile = fs.writeFileSync(levelpath, levelData)
-
-console.log(levelName);
-console.log("Make level script");
